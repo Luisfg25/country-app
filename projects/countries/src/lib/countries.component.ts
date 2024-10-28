e6e0a0c8df57154aca05e3088a65938e73fd0278
Luis Gispert <luis.gispert@xappido.com>
2024-10-28 08:05:12 +0100

    first-commit

---
+++ projects/countries/src/lib/countries.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-countries',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      countries works!
+    </p>
+  `,
+  styles: ``
+})
+export class CountriesComponent {
+
+}
---
+++ projects/countries/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of countries
+ */
+
+export * from './lib/countries.service';
+export * from './lib/countries.component';
